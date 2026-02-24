88fdf76ec41e291d407d7db12d9c3ea3c69de0fd
Kanila Akarsha <156494340+KanilaAkarsha@users.noreply.github.com>
2026-02-24 17:50:15 +0530

    user

--- app/api/v1/login/route.js
+++ app/api/v1/login/route.js
@@ -1,38 +1,60 @@
-import { NextResponse } from "next/server"; 
+import { NextResponse } from "next/server";
 
 const VALID_EMAIL = "kanilasaman1@gmail.com";
 const VALID_PASSWORD = "12345678";
 
-export const POST = async (request) => {
-    
-    try {
-        const { email, password } = await request.json();
-        if (email === VALID_EMAIL && password === VALID_PASSWORD) {
-            return NextResponse.json(
-                { 
-                    success: true,
-                    message: "Login successful" ,
-             }
-            );
-        }else{
-        
-            return NextResponse.json(
-                { 
-                    success: false,
-                    message: "Invalid email or password" ,
-             },
-             { status: 401 }
-            );
-        }
-    } catch (error) {
-        console.error("Login API error:", error);
-        return NextResponse.json(
-            { 
-                success: false,
-                message: "An error occurred during login" ,
-         },
-         { status: 500 }
-        );
+export async function OPTIONS() {
+  return NextResponse.json(
+    {},
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "https://cinescope-mu.vercel.app",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    },
+  );
+}
 
+const allowedOrigin =
+  process.env.NODE_ENV === "production"
+    ? "https://cinescope-mu.vercel.app"
+    : "http://localhost:3000";
+
+export const POST = async (request) => {
+  try {
+    const { email, password } = await request.json();
+    if (email === VALID_EMAIL && password === VALID_PASSWORD) {
+      return NextResponse.json(
+        {
+          success: true,
+          message: "Login successful",
+        },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": allowedOrigin,
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+          },
+        },
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid email or password",
+        },
+        { status: 401 },
+      );
     }
+  } catch (error) {
+    console.error("Login API error:", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "An error occurred during login",
+      },
+      { status: 500 },
+    );
+  }
 };
